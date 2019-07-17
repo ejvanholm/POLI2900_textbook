d98818dee1bf123f0d5c39f61240770709232b91
ejvanholm <35114042+ejvanholm@users.noreply.github.com>
2019-07-17 13:31:27 -0700

    Create _rmarkdown_settings.R

---
+++ bookdown/_rmarkdown_settings.R
@@ -0,0 +1,13 @@
+    
+knitr::opts_chunk$set(
+  warning = FALSE,
+  message = FALSE,
+  comment = "###",
+  collapse = TRUE,
+  cache = FALSE,
+  out.width = "80%",
+  fig.align = 'center',
+  fig.width = 8,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
